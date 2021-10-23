32a921d1ccee4eb9c91c11b8272438d18372758a
ducchien2000 <nguyenducchien25122000@gmail.com>
2021-10-23 19:15:19 +0700

    first commit

---
+++ include/dichvu/gioithieu.php
@@ -0,0 +1,53 @@
+<div style="width:587px; margin-left:3px; margin-right:3px;">
+	<table width="587" border="0" cellpadding="0" cellspacing="0">
+		<tr>
+			<td style="height:25px; background:url(images/trang.jpg) repeat-x;" align="center" class="ht" colspan="3">
+				Giới Thiệu
+			</td>
+		</tr>
+		<tr>
+				<td>
+				<table width="587" height="120" cellpadding="0" cellspacing="0" border="0" style="border:#CCCCCC 1px solid; margin-top:3px;">
+					<tr>
+					<td>
+				<p class="pp" align="center">
+					<img src="images/gioithieu.jpg" border="0" width="200" height="166">
+				</p>	
+				<p class="pp">
+				<span class="demuc">Lời Giới Thiệu:</span>
+				<br />
+Các bạn đã không ít lần phải suy nghĩ, băn khoăn khi không biết nên mua các loại sản phẩm, tốt và chất lượng ở đâu.
+<br />
+				</p>
+				<p class="pp">
+Vậy nêm web chúng tôi cung cấp các sản phẩm tốt nhất thị trường hiện nay để các bậc phụ huynh, cha mẹ lựa chọn món quà ưng í để tặng cho các bé.
+				</p>
+				<p class="pp">
+ Tuy không phải món quà có giá trị cao nhưng là món quà thể hiện được sự quan tâm ,tình cảm yêu quý của người tặng với đến với người nhận .
+				</p>
+				<p class="pp">
+Sản phẩm cho trẻ em là sự lựa chọn trang trọng trong các buổi sinh nhật hay các dịp quan trọng mà người lớn muốn bày tỏ sự yêu thương đến với các em nhỏ…
+				</p>
+				<p class="pp">
+Xuất phát từ những suy nghĩ đó tôi muốn xây dựng một website chuyên cung cấp các sản phẩm về đồ chơi hay là các sản phẩm thiết yếu của trẻ em phục vụ cho cuộc sông hằng ngày và làm quà tặng.
+				</p>
+				<p class="pp" align="center">
+					<img src="images/giohang.jpg" width="220" height="62">
+				</p>	
+				<p class="pp">
+ Mong rằng chúng tôi sẽ mang những sản phẩm tốt nhất, những món quà ý nghĩa nhất đến với người thân yêu của bạn trên khắp mọi miền, khoảng cách về không gian và thời gian không còn là vấn đề nữa các bạn ạ.
+ 				</p>
+				<p class="pp">
+Chúng tôi cũng luôn chú trọng quan tâm đến các trường học, điểm bán lẻ, đặc biệt sẽ có giá ưu đãi cho những cơ quan đặt qua theo định kì dài hạn.
+				</p>
+				<p class="pp">
+
+Triết lí kinh doanh của web chúng tôi là mang lại cho quý khách sự hài lòng hơn cả sự mong đợi, bằng chất lượng sản phẩm và dịch vụ tốt.
+</p>
+				</td>
+			</tr>
+		</table>
+				</td>
+		</tr>
+  </table>
+</div>
